e71c0e76a7e6dacbc67ca8cbead78766880095e4
shreyakhamrai2007 <shreyakhamrai7@gmail.com>
2025-03-25 15:15:51 +0530

    Add files via upload

---
+++ DAY 05/To find out the size of inger,float,double,char.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//To find out the size of integer,float,double,char
+int main()
+{
+	printf ("Size of int :%zu bytes\n",sizeof(int));
+	printf ("Size of float :%zu bytes\n",sizeof(float));
+	printf ("Size of double :%zu bytes\n",sizeof(double));
+	printf ("Size of char :%zu bytes\n",sizeof(char));
+	return 0;
+}
